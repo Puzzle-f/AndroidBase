b3446df5ab157aaba07a8576aa9571a51338539d
puzzle-f <puzzleuri@mail.ru>
2020-10-31 14:08:22 +0300

    message

--- app/src/main/java/com/example/my1/MainActivity.java
+++ app/src/main/java/com/example/my1/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.my1;
 
+import androidx.annotation.XmlRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] cities = {"Ростов-на-Дону", "Москва", "Санкт-Петербург", "Сочи", "Хабаровск", "Россошь"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = (Spinner) findViewById(R.id.cities);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, cities);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+    }
+
+    public void show(View view){
+        Toast.makeText(this, "Сейчас +25 градусов (заглушка)", Toast.LENGTH_SHORT).show();
     }
 }
